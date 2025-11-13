635c94d63b02744273113e82c57b580de75d2771
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-11-13 16:52:48 +0700

    add_hero_section_and_navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className='w-screen px-[5%] bg-[#1C0951] border-b-2 border-[#00C074] overflow-hidden h-[7vh] flex justify-between gap-4 items-center'>
+      <Link href={"/"}><Image src={"/navbar/logo-hackfest.webp"} alt='hackfest logo' width={200} height={200} className='w-32 h-auto'></Image></Link>
+      <div className='hidden md:flex text-white font-semibold gap-4 justify-center items-center'>
+        <Link href={"/"}>HOME</Link>
+        <Link href={"/registration"}>REGISTRATION</Link>
+        <Link href={"/login"}>LOGIN</Link>
+      </div>
+      <div className='flex flex-col md:hidden gap-1 justify-center items-center'>
+       <div className='w-4 h-0.5 bg-white'></div>
+       <div className='w-4 h-0.5 bg-white'></div>
+       <div className='w-4 h-0.5 bg-white'></div>
+      </div>
+    </nav>
   )
 }
 
--- src/components/homepage/HeroSection.tsx
+++ src/components/homepage/HeroSection.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
+import React from "react";
+import FaultyTerminal from "../FaultyTerminal";
 
 function HeroSection() {
   return (
-    <div>HeroSection</div>
-  )
+    <div className="relative min-h-screen w-screen flex flex-col justify-center items-center overflow-hidden">
+      <div className="absolute w-full h-full">
+        <FaultyTerminal
+          scale={1.5}
+          gridMul={[2, 1]}
+          digitSize={1.2}
+          timeScale={1}
+          pause={false}
+          scanlineIntensity={1}
+          glitchAmount={1}
+          flickerAmount={1}
+          noiseAmp={1}
+          chromaticAberration={0}
+          dither={0}
+          curvature={0}
+          tint="#037985"
+          mouseReact={true}
+          mouseStrength={0.5}
+          pageLoadAnimation={false}
+          brightness={1}
+        />
+      </div>
+      <div className="relative z-10 text-white font-bold flex flex-col gap-4 items-center justify-center">
+        <h1 className="text-9xl">HACKFEST</h1>
+        <h1 className="text-7xl"><span className="text-green-500">2026</span>CODE FOR GREENER FUTURE</h1>
+      </div>
+    </div>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
