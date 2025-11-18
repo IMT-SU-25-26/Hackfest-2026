98c06a0bb3800f891972a7810c08bd81ea64f8dd
felix <ffelixrichardo@gmail.com>
2025-11-19 00:14:03 +0700

    fix next-auth configuration

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,23 +1,27 @@
-import NextAuth, { DefaultSession } from "next-auth";
-import { JWT } from "next-auth/jwt";
+import { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
+  interface User extends DefaultUser {
+    id: string;
+    team_name: string;
+  }
+
   interface Session {
     user: {
-      team_id?: string;
-      team_name?: string;
+      team_id: string;
+      team_name: string;
     } & DefaultSession["user"];
   }
 
-  interface User {
-    id: string;
+  interface AdapterUser {
+    team_id: string;
     team_name: string;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    team_id?: string;
-    team_name?: string;
+    team_id: string;
+    team_name: string;
   }
 }
