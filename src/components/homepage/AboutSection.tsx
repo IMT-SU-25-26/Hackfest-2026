77f9caf9b67eae055199e9678b0f16bfcd1798db
felix <ffelixrichardo@gmail.com>
2025-11-19 09:11:46 +0700

    update about section

--- src/components/homepage/AboutSection.tsx
+++ src/components/homepage/AboutSection.tsx
@@ -1,22 +1,12 @@
-"use client";
 import Image from "next/image";
+import Link from "next/link";
+import ButtonImg from "../ButtonImg";
 
 export default function AboutSection() {
   return (
     <div className="bg-black">
-      <div className="w-full mt-10 flex justify-center">
-        <Image
-          src="/home/title.webp"
-          alt="Hackfest Title"
-          width={1920}
-          height={1080}
-          className="w-full"
-        />
-      </div>
-
-
-      <div className="flex justify-center gap-10 p-10">
-        <div className="max-w-[800px]">
+      <div className="w-[90%] md:w-[80%] flex flex-col md:flex-row justify-center m-auto gap-[3%] p-10">
+        <div className="w-full">
           <Image
             src="/home/about.webp"
             alt="About Hackfest"
@@ -26,26 +16,15 @@ export default function AboutSection() {
           />
         </div>
 
-        <div className="flex flex-col gap-8">
-          <div className="text-center cursor-pointer transition-transform duration-200 hover:scale-105">
-            <Image
-              src="/home/guidebook.webp"
-              alt="Guidebook"
-              width={400}
-              height={400}
-              className="w-[160px] mx-auto"
-            />
-          </div>
+        {/* Button Section */}
+        <div className="flex flex-row md:flex-col w-auto md:w-[15%] justify-evenly md:justify-between mt-[3%] md:mt-0 items-center">
 
-          <div className="text-center cursor-pointer transition-transform duration-200 hover:scale-105">
-            <Image
-              src="/home/register.webp"
-              alt="Register"
-              width={400}
-              height={400}
-              className="w-[160px] mx-auto"
-            />
-          </div>
+          {/* LINK 1: GUIDEBOOK */}
+          <ButtonImg imgUrl="/about/pc.svg">GUIDEBOOK</ButtonImg>
+
+          {/* LINK 2: REGISTER */}
+          <ButtonImg imgUrl="/about/disk.svg">Register</ButtonImg>
+
         </div>
       </div>
     </div>
