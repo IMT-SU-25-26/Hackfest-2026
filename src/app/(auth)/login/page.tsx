e3a55767dd6c73c33ea470d3e2be8990a9db9d58
felix <ffelixrichardo@gmail.com>
2025-11-20 22:29:29 +0700

    login done

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import LoginForm, { LoginFormHandle } from '@/components/auth/LoginForm'
+import Link from 'next/link';
 import React from 'react'
 
 function LoginPage2() {
@@ -8,13 +9,14 @@ function LoginPage2() {
         <>
             {/* Page Background */}
             <div className="w-full min-h-screen bg-cover bg-center bg-[url('/login/background.svg')] bg-[#090223">
-                <div className='absolute left-0 top-0 w-full h-full bg-[#090223]/50 flex justify-center items-center pt-[7vh]'>
+                <div className='absolute left-0 top-0 w-full min-h-screen bg-[#090223]/50 flex justify-center items-center pt-[7vh]'>
 
                     {/* Login Container BG */}
-                    <div className="relative pt-0 md:pt-[2%] bg-[url('/auth/bgContainer-mobile.svg')] md:bg-[url('/login/outerframe.svg')] aspect-355/472 md:aspect-1187/627 min-w-[400px] md:min-w-[600px] w-[65%] md:w-[70%] max-w-[990px] bg-contain flex flex-col justify-center items-center bg-no-repeat">
+                    <div className="relative pt-0 -top-[3vh] md:pt-[2%] bg-[url('/auth/bgContainer-mobile.svg')] md:bg-[url('/login/outerframe.svg')] aspect-355/472 md:aspect-1187/627 min-w-[400px] md:min-w-[600px] w-[65%] md:w-[70%] max-w-[990px] bg-contain flex flex-col justify-center items-center bg-no-repeat">
                         <p className='block md:hidden glow-pulse text-3xl font-family-audiowide text-[#05B0C1]'>LOGIN</p>
                         <LoginForm ref={formRef} />
                         
+                        <p className='text-sm lg:text-md xl:text-lg text-center font-family-spacemono text-white'>{`Don't have an account?`} <Link href={'/register'} className='text-[#05B0C1]'>Registration</Link></p>
                         {/* Button Login */}
                         <div
                             onClick={() => formRef.current?.submit()}
