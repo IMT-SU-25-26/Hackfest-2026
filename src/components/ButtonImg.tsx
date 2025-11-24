fd12fe10f7fec5376a46648adeb1263629510851
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-11-24 22:45:18 +0700

    done_tugas

--- src/components/ButtonImg.tsx
+++ src/components/ButtonImg.tsx
@@ -14,13 +14,8 @@ function ButtonImg({imgUrl, children}: ButtonImgProps) {
         className="
             group 
             relative
-            border-2
-            border-[#05C174] 
-            w-[20%]
-            md:w-auto
+            w-full
             h-auto
-            md:h-[47%]
-            aspect-square
             flex 
             flex-col 
             items-center 
@@ -28,27 +23,16 @@ function ButtonImg({imgUrl, children}: ButtonImgProps) {
             transition-all 
             duration-300
             hover:shadow-[0_0_25px_#20e3b2]
-            hover:border-[#32ffc8]
             bg-black/20
         "
         >
-        {/* ICON */}
-        <div className="h-[70%] w-full flex items-center justify-center p-2">
             <Image
             src={imgUrl}
             alt="Register Icon"
             width={120}
             height={120}
-            className="h-[60%] w-auto object-contain group-hover:brightness-125 transition"
+            className="h-full w-full object-contain group-hover:brightness-125 transition"
             />
-        </div>
-
-        {/* TEXT */}
-        <div className="border-t-2 border-t-[#05C174] h-[30%] w-full flex items-center justify-center bg-[#05C174]/10">
-            <p className="text-[#20e3b2] font-bold text-[50%] md:text-[50%] lg:text-[55%] xl:text-[78%] tracking-wide font-family-audiowide">
-            {children}
-            </p>
-        </div>
     </Link>
   )
 }
--- src/components/homepage/AboutSection.tsx
+++ src/components/homepage/AboutSection.tsx
@@ -4,26 +4,65 @@ import ButtonImg from "../ButtonImg";
 export default function AboutSection() {
   return (
     <div className="bg-[#0a0a1f]">
-      <div className="w-[90%] md:w-[80%] flex flex-col md:flex-row justify-center m-auto gap-[3%] p-10">
-        <div className="w-full">
+      <div className="m-auto flex w-[98%] flex-col justify-center gap-[3%] p-10 md:w-[79%] md:flex-row">
+        <div className="flex w-full flex-col gap-4">
           <Image
-            src="/home/about.webp"
+            src="/about/about_title.svg"
             alt="About Hackfest"
             width={2200}
             height={1300}
-            className="w-full"
+            className="hidden h-auto w-full md:inline-block"
           />
-        </div>
+          <Image
+            src="/about/title_mobile.svg"
+            alt="About Hackfest"
+            width={2200}
+            height={1300}
+            className="inline-block h-auto w-full md:hidden"
+          />
+          <div className="mt-5 flex w-full flex-col items-center gap-5 md:flex-row md:gap-10">
+            <Image
+              src="/about/description_panel.svg"
+              alt="About Hackfest"
+              width={2200}
+              height={1300}
+              className="hidden h-auto w-full md:inline-block"
+            />
+            <Image
+              src="/about/about_panel_mobile.svg"
+              alt="About Hackfest"
+              width={2200}
+              height={1300}
+              className="inline-block h-auto w-full md:hidden"
+            />
+            <div className="flex h-auto w-full flex-row gap-4 md:flex-col">
+              <ButtonImg imgUrl="/about/guidebook.svg">GUIDEBOOK</ButtonImg>
 
-        {/* Button Section */}
-        <div className="flex flex-row md:flex-col w-auto md:w-[15%] justify-evenly md:justify-between mt-[3%] md:mt-0 items-center">
-
-          {/* LINK 1: GUIDEBOOK */}
-          <ButtonImg imgUrl="/about/pc.svg">GUIDEBOOK</ButtonImg>
-
-          {/* LINK 2: REGISTER */}
-          <ButtonImg imgUrl="/about/disk.svg">Register</ButtonImg>
-
+              {/* LINK 2: REGISTER */}
+              <ButtonImg imgUrl="/about/register.svg">Register</ButtonImg>
+            </div>
+          </div>
+          <Image
+            src="/about/battery.svg"
+            alt="About Hackfest"
+            width={2200}
+            height={1300}
+            className="inline-block h-auto w-full md:hidden"
+          />
+          <Image
+            src="/about/prizes_mobile.svg"
+            alt="About Hackfest"
+            width={2200}
+            height={1300}
+            className="inline-block h-auto w-full md:hidden"
+          />
+          <Image
+            src="/about/prizes_desktop.svg"
+            alt="About Hackfest"
+            width={2200}
+            height={1300}
+            className="hidden h-auto w-full md:inline-block"
+          />
         </div>
       </div>
     </div>
