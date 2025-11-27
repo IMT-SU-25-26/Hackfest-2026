e7ad8e63f3982d7390b7f0809a771aa860a43e0b
felix <ffelixrichardo@gmail.com>
2025-11-27 13:37:55 +0700

    fix bug file name bg timeline

--- src/components/homepage/TimelineSection.tsx
+++ src/components/homepage/TimelineSection.tsx
@@ -82,7 +82,7 @@ function TimelineSection() {
         <div
           className="block md:hidden w-full min-h-[500px] relative"
           style={{
-            backgroundImage: "url('/timeline/background-fee-mobile.svg')",
+            backgroundImage: "url('/images/timeline/background-fee-mobile.svg')",
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
